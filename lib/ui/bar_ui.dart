d038708bc8949f1258ef507e3b1adacc4daac104
lukaskiermeyer <80913379+lukaskiermeyer@users.noreply.github.com>
2025-09-28 18:10:11 +0200

    Web

--- lib/ui/bar_ui.dart
+++ lib/ui/bar_ui.dart
@@ -18,6 +18,7 @@ class BarSelectionScreen extends StatelessWidget {
       return const CashRegisterScreen();
     }
 
+
     return Scaffold(
       appBar: AppBar(
         // Your new logo is included
